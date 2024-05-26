5007c4291c5bb88217bf197b15e2ee8b421527ac
vatchans <vatchans@gmail.com>
2024-05-26 14:19:22 +0530

    Experience Modified

--- src/App.js
+++ src/App.js
@@ -106,14 +106,15 @@ function App() {
     </div>
     <div className='abt'>
       <h1>Experience</h1>
-      <div class="container-fluid main-section bg-grey">
+      <div class="container-fluid main-section bg-grey mt-4">
         <div class="container">
           <div class="row">
             <div class="col-lg col-md-4 col-12">
               <div class="experience">
                 <div class="col-12">
                   <div style={{ display: "flex", alignItems: "center", gap: "1rem" }}> <h4>AYUR AI</h4>
-                    <h5>June, 2023 – Present</h5></div>
+                    <h5>June, 2023 – Present</h5>
+                  </div>
                 </div>
                 <div class="col-12 ex-left">
                   <h4>Full Stack Engineer</h4>
